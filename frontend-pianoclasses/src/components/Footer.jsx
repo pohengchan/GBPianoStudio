3921f85b036633833dbcf494646dacb19136bfb0
Magda <ma.wernik@gmail.com>
2023-03-06 13:13:07 +0100

    footer without shadow

---
+++ frontend-pianoclasses/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/footer.css';
+
+
+function Footer() {
+  return (
+    <>
+    <footer>
+        <p>© 2023 FemCoders. All rights reserved.</p>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
