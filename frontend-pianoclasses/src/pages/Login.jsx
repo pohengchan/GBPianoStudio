5a9faf67cca702c6dd4f384bc55eaf62bbae8b48
Magda <ma.wernik@gmail.com>
2023-03-07 10:58:44 +0100

    footer added in all pages

--- frontend-pianoclasses/src/pages/Login.jsx
+++ frontend-pianoclasses/src/pages/Login.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Footer from '../components/Footer';
 
 function Login() {
     return (
-    <div>L</div>
+        <>
+        <Footer/>
+       </>
     )
 }
 
