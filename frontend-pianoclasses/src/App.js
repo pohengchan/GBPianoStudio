fe642620b00830d378a33433d5628d4b6a96fad3
Magda <ma.wernik@gmail.com>
2023-03-07 15:44:25 +0100

    piano

--- frontend-pianoclasses/src/App.js
+++ frontend-pianoclasses/src/App.js
@@ -3,14 +3,15 @@ import './index';
 import Footer from './components/Footer';
 import Contactbutton from './components/Contactbutton';
 import Textborder from './components/Textborder';
+import Piano from './components/Piano';
 
 function App() {
   return (
     <>
     <Textborder/>
-      <Contactbutton/>
-      <Footer/>
-    
+    <Contactbutton/>
+    <Piano/>
+    <Footer/>
     </>
   );
 }
