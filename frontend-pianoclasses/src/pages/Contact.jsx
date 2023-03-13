dc7b56f0ed6c352272ea7685005b1745ac07edc0
NataliaVorobyeva <nataliavorobyeva5@gmail.com>
2023-03-13 10:53:02 +0100

    update

--- frontend-pianoclasses/src/pages/Contact.jsx
+++ frontend-pianoclasses/src/pages/Contact.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Footer from '../components/Footer';
-
+import FormSignUp from '../components/FormSignUp';
 function Contact() {
   return (
     <>
+    <FormSignUp/>
     <Footer/>
    </>
   )
