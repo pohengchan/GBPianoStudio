115784fbe1a835e8059fa79de5329088f1fa7bc4
Magda <ma.wernik@gmail.com>
2023-03-15 15:52:07 +0100

    link fixed

--- frontend-pianoclasses/src/pages/Register.jsx
+++ frontend-pianoclasses/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Footer from '../components/Footer';
  import FormSignUp from '../components/FormSignUp'
-import { Link } from 'react-router-dom';
+
 
 function App() {
   return (
