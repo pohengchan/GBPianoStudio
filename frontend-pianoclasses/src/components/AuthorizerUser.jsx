819058fe4593c3d53d872f71f05f5e7b62f2a379
JasMarin <yasminamarin17@gmail.com>
2023-03-20 10:16:44 +0100

    Authorizer users

---
+++ frontend-pianoclasses/src/components/AuthorizerUser.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../index.css';
+import '../styles/authorizerUsers.css'
+
+
+function Authorizer() {
+    return (
+    <>
+    <div id='container-father'>
+        <div className='container'>
+        
+        </div>
+    </div>
+    </>
+    )
+  }
+
+  export default Authorizer;
--- frontend-pianoclasses/src/pages/User.jsx
+++ frontend-pianoclasses/src/pages/User.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import AuthorizerUser from '../components/AuthorizerUser';
 
 function User() {
     return (
         <>
         <Navbar/>
+        <AuthorizerUser/>
         <Footer/>
         </>
     )
