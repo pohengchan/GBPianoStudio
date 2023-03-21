d57cb480c1a3e6b7ae40dd1c7b1875bc5c2f5d24
pohengchan <pohengchan@gmail.com>
2023-03-21 09:46:58 +0100

    pass lessons from back to front

--- frontend-pianoclasses/src/components/DayHour/DayHour.jsx
+++ frontend-pianoclasses/src/components/DayHour/DayHour.jsx
@@ -6,27 +6,46 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from "@fullcalendar/interaction";
-import { useState } from "react";
-import { v4 as uuid } from "uuid";
+// import { useState } from "react";
+// import { v4 as uuid } from "uuid";
 
-function DayHour() {
+//bring in api 20 march 2023
+import axios from 'axios';
 
-  const [events, setEvents] = useState([]);
-  const handleSelect = (info) => {
-    const { start, end } = info;
-    const eventNamePrompt = prompt("Enter, event name" + info );
-    if (eventNamePrompt) {
-      setEvents([
-        ...events,
-        {
-          start,
-          end,
-          title: eventNamePrompt,
-          id: uuid(),
-        },
-      ]);
-    }
-  };
+class  DayHour extends React.Component {
+
+//bring in api 20 march 2023
+constructor (){
+  super();
+  this.state = {
+    events: []
+  }
+}
+componentDidMount(){
+  axios.get('http://localhost:8000/api/lesson/1').then(function(response){
+    this.setState({events: response.data});
+}.bind(this)
+
+);
+
+}
+
+  // const [events, setEvents] = useState([]);
+  // const handleSelect = (info) => {
+  //   const { start, end } = info;
+  //   const eventNamePrompt = prompt("Enter, event name" + info );
+  //   if (eventNamePrompt) {
+  //     setEvents([
+  //       ...events,
+  //       {
+  //         start,
+  //         end,
+  //         title: eventNamePrompt,
+  //         id: uuid(),
+  //       },
+  //     ]);
+  //   }
+  // };
     // var calendarEl = document.getElementById('calendar');
   
     // var calendar = new FullCalendar.Calendar(calendarEl, {
@@ -50,7 +69,7 @@ function DayHour() {
     // });
   
     // calendar.render();
-
+render(){
         return (
             
           <div>
@@ -60,7 +79,7 @@ function DayHour() {
             editable
             selectable
             //events={events}
-            select={handleSelect}
+           // select={handleSelect}
             plugins={[timeGridPlugin, dayGridPlugin, interactionPlugin]}
               // plugins= {[ 'interaction', 'dayGrid', 'timeGrid' ]}
             defaultView= 'timeGridWeek'
@@ -78,18 +97,19 @@ function DayHour() {
               //   right: 'dayGridMonth,timeGridWeek,timeGridDay'
               // }}
               events={[
-                { title: 'unavailable', start: '2023-03-13T10:00:00' , end: '2023-03-13T15:00:00', backgroundColor: 'grey'},
-                { title: 'unavailable', start: '2023-03-14T10:00:00' , end: '2023-03-14T15:15:00', backgroundColor: 'grey'},
-                { title: 'unavailable', start: '2023-03-15T10:00:00' , end: '2023-03-15T14:15:00', backgroundColor: 'grey'},
-                { title: 'unavailable', start: '2023-03-16T10:00:00' , end: '2023-03-16T15:15:00', backgroundColor: 'grey'},
-                { title: 'unavailable', start: '2023-03-17T10:00:00' , end: '2023-03-17T14:15:00', backgroundColor: 'grey'},
-                { title: 'unavailable', start: '2023-03-18T09:00:00' , end: '2023-03-18T21:00:00', backgroundColor: 'grey'},
+                this.state.events
+                // { title: 'unavailable', start: '2023-03-13T10:00:00' , end: '2023-03-13T15:00:00', backgroundColor: 'grey'},
+                // { title: 'unavailable', start: '2023-03-14T10:00:00' , end: '2023-03-14T15:15:00', backgroundColor: 'grey'},
+                // { title: 'unavailable', start: '2023-03-15T10:00:00' , end: '2023-03-15T14:15:00', backgroundColor: 'grey'},
+                // { title: 'unavailable', start: '2023-03-16T10:00:00' , end: '2023-03-16T15:15:00', backgroundColor: 'grey'},
+                // { title: 'unavailable', start: '2023-03-17T10:00:00' , end: '2023-03-17T14:15:00', backgroundColor: 'grey'},
+                // { title: 'unavailable', start: '2023-03-18T09:00:00' , end: '2023-03-18T21:00:00', backgroundColor: 'grey'},
               ]}
               
             />
             </div>
           </div>
         );
-
+            };
     };
 export default DayHour;
