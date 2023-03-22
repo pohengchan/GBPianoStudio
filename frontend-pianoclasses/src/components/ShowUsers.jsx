4645535158b4f892f08317a2b797393dc47a9971
JasMarin <yasminamarin17@gmail.com>
2023-03-22 22:01:20 +0100

    Users render and styles

--- frontend-pianoclasses/src/components/ShowUsers.jsx
+++ frontend-pianoclasses/src/components/ShowUsers.jsx
@@ -1,79 +1,101 @@
-import React, {useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import '../styles/showUsers.css';
 
-import axios from 'axios'
-import '../styles/showUsers.css'
-
-const endpoint = 'http://localhost:8000/api'
+const endpoint = 'http://localhost:8000/api';
 
 const ShowUsers = () => {
+const [users, setUsers] = useState([]);
+const [selectedUser, setSelectedUser] = useState(null);
+const [showModal, setShowModal] = useState(false);
 
-    const [users, setUsers ] = useState( [] )
+useEffect(() => {
+    getAllUsers();
+}, []);
 
-    useEffect ( () => {
-        getAllUsers()
-    }, [])
+const getAllUsers = async () => {
+    const response = await axios.get(`${endpoint}/users`);
+    setUsers(response.data);
+};
 
-    const getAllUsers = async() => {
-        const response = await axios.get(`${endpoint}/users`)
-        setUsers(response.data)
-    }
+const getUserDetails = async (id) => {
+    const response = await axios.get(`${endpoint}/users/${id}`);
+    setSelectedUser(response.data);
+    setShowModal(true);
+};
 
-    const deleteUser = async(id) => {
-        await axios.delete(`${endpoint}/users/{id}`)
-        getAllUsers()
-    }
+const closeModal = () => {
+    setSelectedUser(null);
+    setShowModal(false);
+};
 
-  return (
-<div>
-    { <h1 className='h1-users'>Users</h1> }
-    
-    <div className='container'>
-    <div className='tableUsers'>
-        {/* { <div className='create'>
-        <Link to='/create' className='custom-btn btn-15'>Create</Link>
-        </div> } */}
-        <table className='table'>
-    
-        <thead className='head'>
+return (
+    <div>
+    <h1 className="h1-users">Users</h1>
+    <div className="container">
+        <div className="tableUsers">
+        <table className="table">
+            <thead className="head">
             <tr>
-                <th>Name Parent´s</th>
+                {/* <th>Name Parent´s</th> */}
                 <th>Student</th>
                 <th>Email</th>
                 <th>Phone</th>
                 <th>Authorized User</th>
+                <th>Management</th>
             </tr>
-        </thead>
-
-        <tbody>
+            </thead>
+            <tbody>
             {users.map((user) => (
                 <tr key={user.id}>
-                    <td><p>{user.contact_name}</p></td>
-                    <td><p>{user.student_name}</p></td>
-                    <td><p>{user.email}</p></td>
-                    <td><p>{user.phone_number}</p></td>
-                    <td>
-                        <input type="checkbox" className='Checkbox' id={`user-${user.id}`} />
-                    </td>
+                {/* <td>
+                    <p>{user.contact_name}</p>
+                </td> */}
+                <td>
+                    <p>{user.student_name}</p>
+                </td>
+                <td>
+                    <p>{user.email}</p>
+                </td>
+                <td>
+                    <p>{user.phone_number}</p>
+                </td>
+                <td>
+                    <input type="checkbox" className="Checkbox" id={`user-${user.id}`} />
+                </td>
+                <td>
+                    <button value="details" onClick={() => getUserDetails(user.id)}>
+                    Details
+                    </button>
+                </td>
                 </tr>
             ))}
-        </tbody>
-    </table>
-
-    <div className='button-container'>
-        {users.map((user) => (
-            <div key={user.id} className='button-group'>
-            {/* { <Link to={`/edit/${user.id}`} className='btnedit'>Edit</Link> } */}
-            <button as={Link} to={`/edit/${user.id}`} className='btnedit'>Edit</button>
-            <button onClick={() => deleteUser(user.id)} className='btndelete'>Delete</button>
-            </div>
-        ))}
+            </tbody>
+        </table>
+        </div>
     </div>
-</div>
-</div>
-</div>
+    {showModal && (
+        <div className="modal">
+        <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+            &times;
+            </span>
+            <div>
+            <h2>User Details</h2>
+            <p>Name: {selectedUser.contact_name}</p>
+            <p>Student: {selectedUser.student_name}</p>
+            <p>Email: {selectedUser.email}</p>
+            <p>Phone: {selectedUser.phone_number}</p>
+            <p>Phone: {selectedUser.date_of_birth}</p>
+            <p>Phone: {selectedUser.candidate_number}</p>
+            </div>
+        </div>
+        </div>
+    )}
+    </div>
+    
+);
+};
 
-  )
-}
 
-export default ShowUsers
+export default ShowUsers;
