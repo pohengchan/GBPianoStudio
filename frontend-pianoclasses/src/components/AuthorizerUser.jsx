3356e96e8af9db1d8d7eb256bc8eec6e003de458
pohengchan <pohengchan@gmail.com>
2023-03-31 01:19:15 +0200

    authorise user

--- frontend-pianoclasses/src/components/AuthorizerUser.jsx
+++ frontend-pianoclasses/src/components/AuthorizerUser.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 import '../styles/authorizerUsers.css'
 import { useState } from "react";
 import DayHour from './DayHour/DayHour';
+import { getAxiosInstance } from '../services/functions';
+var instance = getAxiosInstance();
 
 function Authorizer({ user }) {
     const [isChecked, setIsChecked] = useState(false);
   
     const handleCheckboxChange = (event) => {
+    handleAuthorise(user);    
     setIsChecked(event.target.checked);
     }
+    const handleAuthorise = async (id) => {
+        try {
+            // const result = await axios.post(`YOUR_URL`, {<Your JSON payload>});
+            const result = await instance.put(`http://localhost:8000/api/users/${user}/authorize`, {
+                });
+            console.log(result);
+            console.log(instance);
+          } catch (error) {
+            console.error(error);
+          }
+
+    };
 
     if (!user) {
     return null; 
