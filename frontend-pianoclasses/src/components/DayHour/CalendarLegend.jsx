e31a2b6878b12e6cdac47ea7eb1db03f363eb3bc
pohengchan <pohengchan@gmail.com>
2023-04-01 01:16:01 +0200

    adding legend

--- frontend-pianoclasses/src/components/DayHour/CalendarLegend.jsx
+++ frontend-pianoclasses/src/components/DayHour/CalendarLegend.jsx
@@ -8,13 +8,21 @@ export default function CalendarLegend () {
   return (
     <>
             
-            <div className="legend-title"><p className="legend-title">Colour code: </p></div>
+        <div><p className="legend-title">Colour code: </p></div>
+        <div className='legend'>
             <div className='legend'>
-            <FontAwesomeIcon icon={faMusic} style={{color: "#f313f6",}} />
+                <FontAwesomeIcon className='legend-icon i-blue' icon={faMusic} style={{color: "white",}} />
+                <p className='legend-p'>Confirmed</p>
             </div>
-            
-           
-          
+            <div className='legend'>
+                <FontAwesomeIcon className='legend-icon i-pink' icon={faMusic} style={{color: "white",}} />
+                <p className='legend-p'>To be confirmed</p>
+            </div>
+            <div  className='legend'>
+                <FontAwesomeIcon className='legend-icon i-grey' icon={faMusic} style={{color: "white",}} />
+                <p className='legend-p'>Unavailable</p>
+            </div>
+        </div>
     </>
   )
 }
