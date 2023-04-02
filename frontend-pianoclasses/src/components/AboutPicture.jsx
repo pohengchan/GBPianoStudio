11eca0dbd48fbec2bed9dcd0980fc5499a23ac4d
pohengchan <pohengchan@gmail.com>
2023-04-02 14:28:12 +0200

    retouching about

--- frontend-pianoclasses/src/components/AboutPicture.jsx
+++ frontend-pianoclasses/src/components/AboutPicture.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../index.css';
-import photo from '../assets/images/piano_front.jpeg'; // with import
+import photo from '../assets/images/piano_front.png'; // with import
 
 
 function AboutPicture() {
   return (
     <>
     <div id='container-father-box'>
-        <img style={{ width: "40%", height: "40%" }} src={photo} alt="The class piano" />
+        <img className="p-bottom-padding" style={{ width: "40%", height: "40%"}} src={photo} alt="The class piano" />
     </div>
     </>
   )
